8fa0264f0f43f37d799447cc135e7ee43cf23125
Chetan1330 <41874307+Chetan1330@users.noreply.github.com>
2022-10-30 16:19:02 +0530

    Initial

---
+++ apps/home/urls.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.urls import path, re_path
+from apps.home import views
+
+urlpatterns = [
+
+    # The home page
+    path('', views.index, name='home'),
+    path('', views.index, name='index'),
+    path("folder/<int:folderid>/",views.folder, name="folder"),
+    path("addFolder/", views.addfolder, name="addfolder"),
+    path("delete/<int:deleteid>/",views.delete, name="delete"),
+    path("image/", views.image, name="image"),
+
+    re_path(r'^.*\.*', views.pages, name='pages'),
+
+]
